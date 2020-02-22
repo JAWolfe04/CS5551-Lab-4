9c8d54fb2bcd9a23561da60b29ca4f2b4b4415bd
Jonathan <jawhf4@mail.umkc.edu>
2020-02-21 23:52:13 -0600

    Added Documentation

--- App/src/app/service/auth-response.ts
+++ App/src/app/service/auth-response.ts
@@ -1,3 +1,25 @@
+//-----------------------------------------------------------------------------------------------------------------
+// CS 5551
+// Lab 4
+// Jonathan Wolfe
+// Bill Yerkes
+// Create a simple ionic application using any of the ionic templates [tabs, sidemenu]
+//     Make changes to the template to display
+//     1. Login and Registration Activities
+//     The application must have a sign up and login activities.
+//
+//     2. A Main page
+//     The Page should be Mashup of at least Two Web Services (refer to the web services
+//     from the spreadsheet). One of them should be from the list of knowledge/Machine
+//     learning/AI services and that you haven’t used in your previous work.
+//
+//     3. Testing
+//     Write at least 3-unit test cases related to your application.
+//
+//     Deploy the application to android device/ web/ ionic lab
+//
+// Adapted from https://www.techiediaries.com/ionic/ionic-5-jwt-authentication-node-expressjs/
+//-----------------------------------------------------------------------------------------------------------------
 export interface AuthResponse {
     user: {
         id: number,
